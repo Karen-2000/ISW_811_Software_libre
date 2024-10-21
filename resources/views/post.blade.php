6c0b7e9cd4fc2f6eace7943ae3b15d91020f9929
Karen-2000 <karen2000pje@gmail.com>
2024-10-21 14:31:00 -0600

    working sunday- teh basics

---
+++ resources/views/post.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<link rel="stylesheet" href="app.css">
+<title>My Blog</title>
+<body> 
+    <?php foreach ($posts as $post): ?>
+        <article>
+            <h1>
+                <a href="/posts/<?= $post->slug; ?>">
+                    <?= $post->title; ?>
+                </a>
+            </h1>
+            <div>
+            <?= $post->$excerpt; ?>
+            </div>
+        </article>
+        <?php endforeach; ?>
+</body>
--- routes/web.php
+++ routes/web.php
@@ -1,21 +1,23 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Illuminate\Support\Facades\File; 
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 Route::get('/', function () {
-    return view('welcome');
+
+    return view('posts', [
+        'posts' => Post::all()
+    ]);
+    
 });
+
+Route::get('posts/{post}', function ($slug) {
+    return view('post', [
+        'post' => Post::find($slug)
+    ]);
+})->where('post', '[A-z_\-]+');
 
 Auth::routes();
 
